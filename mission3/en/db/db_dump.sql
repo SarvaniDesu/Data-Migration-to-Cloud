e5eb856ab315b66f81f204d7df873783def7dacd
SarvaniDesu <46963859+SarvaniDesu@users.noreply.github.com>
2024-02-05 17:02:12 -0700

    Update db_dump.sql

--- mission3/en/db/db_dump.sql
+++ mission3/en/db/db_dump.sql
@@ -31,7 +31,7 @@ CREATE TABLE `records` (
   `pdf` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `records`
